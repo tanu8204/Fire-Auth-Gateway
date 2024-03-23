f75a2b8b8a26cff315e65b2d0b105dbe8be13307
tanu8204 <161141310+tanu8204@users.noreply.github.com>
2024-03-23 21:42:48 +0530

    Update FireBaseInitization.java

--- src/main/java/com/example/FireAuthGateway/FireBaseInitialization/FireBaseInitization.java
+++ src/main/java/com/example/FireAuthGateway/FireBaseInitialization/FireBaseInitization.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @Service
 public class FireBaseInitization {
 
-    @PostConstruct
+    /*@PostConstruct
     public void initialization() {
         FileInputStream serviceAccount =
                 null;
@@ -27,6 +29,30 @@ public class FireBaseInitization {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+    }*/
+
+        private static boolean firebaseInitialized = false;
+        @PostConstruct
+        public void initialization() {
+            if (!firebaseInitialized) {
+                try {
+                    FileInputStream serviceAccount = new FileInputStream("./serviceAccountKey.json");
+                    FirebaseOptions options = new FirebaseOptions.Builder()
+                            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                            .build();
+                    FirebaseApp.initializeApp(options);
+                    firebaseInitialized = true;
+                } catch (FileNotFoundException e) {
+                    System.err.println("Service account JSON file not found.");
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    System.err.println("Error reading service account JSON file.");
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    System.err.println("Error initializing Firebase.");
+                    e.printStackTrace();
+                }
+            }
+        }
 
 }
