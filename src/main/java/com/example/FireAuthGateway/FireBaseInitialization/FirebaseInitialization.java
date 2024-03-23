e05cdf593083fe8e74420f4e00c960ac155f4475
tanu8204 <161141310+tanu8204@users.noreply.github.com>
2024-03-24 02:13:36 +0530

    Add files via upload

---
+++ src/main/java/com/example/FireAuthGateway/FireBaseInitialization/FirebaseInitialization.java
@@ -0,0 +1,41 @@
+package com.example.FireAuthGateway.FireBaseInitialization;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class FirebaseInitialization {
+
+    private static final Map<String, FirebaseApp> instances = new HashMap<>();
+
+    @PostConstruct
+    public void initialization() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("./serviceAccountKey.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp app = FirebaseApp.initializeApp(options, "myCustomAppName");
+
+            synchronized (instances) {
+                instances.put("myCustomAppName", app);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
