25e755468003d59381482f5ce1837d64f21aabb4
tanu8204 <161141310+tanu8204@users.noreply.github.com>
2024-03-23 21:31:48 +0530

    Update FireBaseInitization.java

--- src/main/java/com/example/FireAuthGateway/FireBaseInitialization/FireBaseInitization.java
+++ src/main/java/com/example/FireAuthGateway/FireBaseInitialization/FireBaseInitization.java
@@ -7,28 +7,26 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 @Service
 public class FireBaseInitization {
 
     @PostConstruct
-    public void initializeFirestore() throws IOException, FileNotFoundException {
+    public void initialization() {
         FileInputStream serviceAccount =
-                new FileInputStream("./serviceAccountKey.json");
+                null;
+        try {
+            serviceAccount = new FileInputStream("./serviceAccountKey.json");
+
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
 
-        // Clear existing FirebaseApp instances
-        for (FirebaseApp app : FirebaseApp.getApps()) {
-            app.delete();
-        }
-
-        // Initialize FirebaseApp with the new options
         FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
