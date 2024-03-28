6f7ba01a160c94c8e11d13f960f95724e99816e3
tanu8204 <161141310+tanu8204@users.noreply.github.com>
2024-03-29 02:27:56 +0530

    Add files via upload

---
+++ src/main/java/com/example/FireAuthGateway/FireBaseInitialization/FirebaseInitialization.java
@@ -0,0 +1,37 @@
+package com.example.FireAuthGateway.FireBaseInitialization;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class FirebaseInitialization {
+
+    @Async
+    @PostConstruct
+    public CompletableFuture<Void> initializationAsync() {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                FileInputStream serviceAccount = new FileInputStream("./serviceAccountKey.json");
+
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+
+                FirebaseApp.initializeApp(options);
+                System.out.println("Firebase initialized successfully");
+                System.out.println(options);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new IllegalStateException("Error initializing Firebase", e);
+            }
+        });
+    }
+}
